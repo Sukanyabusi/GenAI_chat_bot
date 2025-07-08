34c8e6fb77883c75448f595c01cdde2d44b0c41e
SUKANYA_BUSI <sukanya@unisonconsulting.com.sg>
2025-07-08 11:08:59 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'sukanyabusi',
+    author_email= 'sukanyabusi989@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
